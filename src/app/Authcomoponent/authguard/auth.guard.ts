c6dcc017fe46b37ba6c780c605d782efe3eb208f
ksg0298 <sg.sourav0298@gmail.com>
2023-04-02 03:11:52 +0530

    auth

--- src/app/Authcomoponent/authguard/auth.guard.ts
+++ src/app/Authcomoponent/authguard/auth.guard.ts
@@ -6,9 +6,7 @@ import { AuthService } from '../services/auth.service';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor( private authSrvice:AuthService){}
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(): boolean{
     if(this.authSrvice.isAuthenticate){
       return true;
     }
