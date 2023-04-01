6ebe9322e99694aa34e05ec6c38f00da180bc39a
ksg0298 <sg.sourav0298@gmail.com>
2023-04-02 02:27:08 +0530

    auth guard impl

--- src/app/Authcomoponent/authguard/auth.guard.ts
+++ src/app/Authcomoponent/authguard/auth.guard.ts
@@ -6,9 +6,9 @@ import { AuthService } from '../services/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivateChild {
+export class AuthGuard implements CanActivate {
   constructor( private authSrvice:AuthService){}
-  canActivateChild(
+  canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if(this.authSrvice.isAuthenticate){
