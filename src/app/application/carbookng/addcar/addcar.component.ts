4a133d909af3a66401de8b19c83ce6ee7e284463
vivek jha <vivek.jha@csm.co.in>
2023-04-18 00:45:26 +0530

    changes

--- src/app/application/carbookng/addcar/addcar.component.ts
+++ src/app/application/carbookng/addcar/addcar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-addcar',
   templateUrl: './addcar.component.html',
   styleUrls: ['./addcar.component.css']
 })
-export class AddcarComponent {
+export class AddcarComponent implements OnInit {
+
+  tooltipList: any;
+
+  ngOnInit(): void {
+    // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    //   return new bootstrap.Tooltip(tooltipTriggerEl)
+    // })
+  }
 
 }
